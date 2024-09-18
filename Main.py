cae836edc84d12214909470cdd792fb56f06b730
Thomas M. McBride <mcbride.t@northeastern.edu>
2024-09-17 23:17:14 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,24 +14,24 @@ class Model:
     def __init__(self, possible_words):
         self.possible_words = possible_words
         self.word_to_guess = random.choice(self.possible_words)
-        self.correct_letters = ["_"] * len(self.word_to_guess)
+        self.word_so_far = ["_"] * len(self.word_to_guess)
         self.incorrect_letters = []
-        self.num_attempts = 10
+        self.lives = 10
     
     def guess_letter(self, letter):
-        if letter.lower() in self.word_to_guess:
+        if letter in self.word_to_guess:  
             for i, char in enumerate(self.word_to_guess):
                 if letter == char:
-                    self.correct_letters[i] = char
+                    self.word_so_far[i] = char
         else:
-            self.incorrect_letters.append(letter.lower())
-            self.num_attempts -= 1
+            self.incorrect_letters.append(letter)
+            self.lives -= 1
     
     def game_over(self):
-        return self.num_attempts == 0 and "_" in self.correct_letters
+        return self.lives == 0 and "_" in self.word_so_far
     
     def player_wins(self):
-        return self.num_attempts > 0 and "_" not in self.correct_letters
+        return self.lives > 0 and "_" not in self.word_so_far
     
 
 # View: Displays information about the game to the player, and receives input from the player
@@ -60,7 +60,7 @@ class View:
     
     @staticmethod
     def get_next_guess():
-        return input("\nWhat is your next letter guess?:")
+        return input("\nWhat is your next letter guess?: ")
     
     @staticmethod
     def display_try_again():
@@ -74,160 +74,170 @@ class View:
     def show_man(stage):
         stages = [
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                      \|||/ 
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                  \|||/ 
+                   \o/
+                    |
+                   / \\
 
              
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                      \| |/ 
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                  \| |/ 
+                   \o/
+                    |
+                   / \\
 
                 
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                       \|/ 
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                   \|/ 
+                   \o/
+                    |
+                   / \\
 
                      
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                       | |
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                   | |
+                   \o/
+                    |
+                   / \\
 
                      
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                       | |
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                   | |
+                   \o/
+                    |
+                   / \\
 
                      
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                        |
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                    |
+                   \o/
+                    |
+                   / \\
 
                       
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                   \o/
+                    |
+                   / \\
 
                   
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                      | | |    
-                       \o/
-                        |
-                       / \\
 
-            
-            |-
-            |  \\_
-        ____|_____\\____________
+                 _______
+               /         \\
+              |           |
+              |___________|
+                  | | |    
+                   \o/
+                    |
+                   / \\
+
+           
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                       | |     
-                       \o/
-                        |
-                       / \\
+        
+                 _______
+               /         \\
+              |           |
+              |___________|
+                   | |     
+                   \o/
+                    |
+                   / \\
 
               
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                       |    
-                       \o/
-                        |
-                       / \\
+
+                 _______
+               /         \\
+              |           |
+              |___________|
+                  |    
+                   \o/
+                    |
+                   / \\
 
                
-            |-
-            |  \\_
-        ____|_____\\____________
+        |-
+        |  \\_
+    ____|_____\\____________
         """,
         """ 
 
@@ -239,176 +249,176 @@ class View:
 
 
 
-           mmm food...       
-            |-
-            |  \\_      uh oh...
-        ____|_____\\_____\o/____
+       mmm food...       
+        |-
+        |  \\_      uh oh...
+    ____|_____\\_____\o/____
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                      \|||/ 
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                  \|||/ 
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+    whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                      \| |/ 
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                  \| |/ 
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+    whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                       \|/ 
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                   \|/ 
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+    whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ ||| / 
-                       | |
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ ||| / 
+                   | |
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+    whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                       | |
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                   | |
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                        |
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                    |
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                     \ | | / 
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                 \ | | / 
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                      | | |    
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                  | | |    
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                       | |     
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                   | |     
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """,
         """
-                      phew!
-                     _______
-                   /         \\
-                  |           |
-                  |___________|
-                       |    
-                       \o/
-                        |
-                       / \\
+                  phew!
+                 _______
+               /         \\
+              |           |
+              |___________|
+                   |    
+                   \o/
+                    |
+                   / \\
 
-        whatever...    
-             -|
-          _/  |
-        /_____|__________________
+   whatever...    
+          -|
+       _/  |
+     /_____|__________________
         """]
 
         print(stages[stage])
@@ -423,13 +433,14 @@ class Controller:
         self.view = view
     
     def go(self):
+        self.view.print_space()
         self.view.display_welcome()
 
         while not self.model.game_over() and not self.model.player_wins():
-            self.view.show_man(10 - self.model.num_attempts)
-            self.view.display_text(f"Word guessed so far: {' '.join(self.model.correct_letters)}")
+            self.view.show_man(10 - self.model.lives)
+            self.view.display_text(f"Word guessed so far: {' '.join(self.model.word_so_far)}")
             self.view.display_text(f"Incorrect guesses: {', '.join(self.model.incorrect_letters)}")
-            self.view.display_text(f"Number of guesses left: {self.model.num_attempts}")
+            self.view.display_text(f"Number of lives left: {self.model.lives}")
             guess = self.view.get_next_guess()
 
             if len(guess) != 1 or not guess.isalpha():
@@ -437,15 +448,16 @@ class Controller:
                 self.view.display_try_again()
                 continue
 
-            self.model.guess_letter(guess)
+            self.model.guess_letter(guess.lower())
+            self.view.print_space()
         
         if self.model.game_over():
             self.view.show_man(10)
             self.view.display_game_over()
         elif self.model.player_wins():
-            self.view.show_man(21 - self.model.num_attempts)
-            self.view.display_text(f"Number of attempts: {10 - self.model.num_attempts}")
-            self.view.display_text(f"Final word: {''.join(self.model.correct_letters)}")
+            self.view.show_man(21 - self.model.lives)
+            self.view.display_text(f"Number of lives lost: {10 - self.model.lives}")
+            self.view.display_text(f"Final word: {''.join(self.model.word_so_far)}")
             self.view.display_game_won()
 
 
